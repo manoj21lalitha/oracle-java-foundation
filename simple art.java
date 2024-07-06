a0eb7aa458e68c00c39978db3509205bfa74a0fd
MANOJKUMAR S <145280556+manoj21lalitha@users.noreply.github.com>
2024-07-06 12:51:26 +0530

    oracle task simple ascii art

---
+++ simple art.java
@@ -0,0 +1,9 @@
+public class SimpleAsciiArt {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___");
+        System.out.println(" ( _ \\/ _ )");
+        System.out.println("  )   / _ \\/\\");
+        System.out.println(" ( (  (  (_) )");
+        System.out.println("  ))  \\/ ");
+    }
+}
